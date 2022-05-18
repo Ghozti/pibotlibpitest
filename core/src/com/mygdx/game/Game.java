861575b3d0ffd5e3b67d940d5cdc41851505b3f1
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-05-17 21:34:34 -0400

    - working version

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -5,15 +5,36 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalOutputConfigBuilder;
+import com.pi4j.io.gpio.digital.DigitalState;
 
 public class Game extends ApplicationAdapter {
+
 	SpriteBatch batch;
 	Texture img;
+	Context context;
+	//Pwm pwm;
+	DigitalOutputConfigBuilder pinConfig;
+	DigitalOutput pin;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		context = Pi4J.newAutoContext();
+		pinConfig = DigitalOutput.newConfigBuilder(context)
+				.id("led")
+				.name("LED Flasher")
+				.address(4)
+				.shutdown(DigitalState.LOW)
+				.initial(DigitalState.LOW)
+				.provider("pigpio-digital-output");
+		pin = context.create(pinConfig);
+		//pwm = context.create(buildPwmConfig(context,12));
+		//System.out.println("pwm init");
 	}
 
 	@Override
